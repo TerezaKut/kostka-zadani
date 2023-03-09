682c3f7cddb94736addb775ec4fe669e4ab0df87
Tereza Kutinova <terezakutin@gmail.com>
2023-03-09 23:07:10 +0100

    DU

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log('funguju!');
+
+let pocitadlo = 1;
+let kostka = document.querySelector('.dice');
+
+function zmenKostku(){
+    if (pocitadlo < 6){
+        pocitadlo += 1;
+
+    } else {
+        pocitadlo=1;
+    }
+
+    kostka.src = 'img/side'+pocitadlo+'.svg';
+}
+
+document.addEventListener('keydown', zmenKostku);
